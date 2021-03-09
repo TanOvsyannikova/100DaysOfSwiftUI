be6cfa41392519b6f82b228956c5c1bc6c1bef8b
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-09 21:06:48 +0300

    initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Татьяна Овсянникова on 08.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
