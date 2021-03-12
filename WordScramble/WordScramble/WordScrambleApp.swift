2b4131f1befbed1ba48be96264b48a444cd4ceda
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-12 22:34:30 +0300

    initial commit

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Татьяна Овсянникова on 11.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
