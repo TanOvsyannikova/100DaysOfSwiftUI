37781f122f7e357420998b0af4d776dcadec6797
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-17 12:49:04 +0300

    feat(animations): add wrong answer animations

--- GuessTheFlag2/GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/GuessTheFlag2/ContentView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    @State private var animated = false
-    @State var selected = false
+    @State private var animatedCorrectAnswer = false
+    @State private var animatedWrongAnswer = false
+    @State private var wrongAnswer = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
@@ -20,6 +22,8 @@ struct ContentView: View {
     
     @State private var score = 0
     @State private var counter = 1
+    
+    let generator = UINotificationFeedbackGenerator()
     
     let totalQuestions = 10
     
@@ -37,13 +41,23 @@ struct ContentView: View {
                 
                 ForEach (0 ..< 3) { number in
                     Button(action: {
-                        withAnimation(.easeOut(duration: 0.5)) {
-                            self.animated = true
+                        if flagTappedIsCorrect(number) {
+                            withAnimation(.easeOut(duration: 0.5)) {
+                                self.generator.notificationOccurred(.success)
+                                self.animatedCorrectAnswer = true
+                            }
+                        } else {
+                            withAnimation(.interpolatingSpring(stiffness: 10, damping: 5)) {
+                                self.generator.notificationOccurred(.error)
+                                                           self.animatedWrongAnswer = true
+                                                           self.wrongAnswer = number
+                                                       }
                         }
+                        
                         
                     
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                         flagTapped(number)
                     }
                     
@@ -54,9 +68,10 @@ struct ContentView: View {
                             .border(Color.gray, width: 1)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                             .shadow(radius: 10)
-                            .rotation3DEffect(.degrees(flagTappedIsCorrect(number) && animated ? 360 : 0.0), axis: (x: 0, y: 1, z: 0))
-                            .opacity(!flagTappedIsCorrect(number) && animated ? 0.25 : 1)
-                        
+                            .rotation3DEffect(.degrees(flagTappedIsCorrect(number) && animatedCorrectAnswer ? 360 : 0.0), axis: (x: 0, y: 1, z: 0))
+                            .opacity(!flagTappedIsCorrect(number) && animatedCorrectAnswer ? 0.25 : 1)
+                            .overlay(number == wrongAnswer && self.animatedWrongAnswer ? RoundedRectangle(cornerRadius: 10).fill(Color.red) : nil)
+                            .scaleEffect(number == wrongAnswer && self.animatedWrongAnswer ? 0.95 : 1)
                         
                     }
                 
@@ -100,7 +115,8 @@ func flagTapped(_ number: Int) {
 }
 
 func askQuestion() {
-    animated = false
+    animatedCorrectAnswer = false
+    animatedWrongAnswer = false
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
 }
