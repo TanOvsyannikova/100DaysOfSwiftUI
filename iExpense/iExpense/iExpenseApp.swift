72aab57ebea0ec4d4750c0152657aa71d5cd20f3
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-19 22:17:42 +0300

    initial commit

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Татьяна Овсянникова on 18.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
