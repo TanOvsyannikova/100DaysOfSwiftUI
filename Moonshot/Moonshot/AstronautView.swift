bc108d428018bec37e5dd0cd58e2db4095c8cf80
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-24 18:48:05 +0300

    feat: add bar button 'show dates/show crew'

--- Moonshot/Moonshot/AstronautView.swift
+++ Moonshot/Moonshot/AstronautView.swift
@@ -9,22 +9,51 @@ import SwiftUI
 
 struct AstronautView: View {
     let astronaut: Astronaut
-    
+    var missionsFlown = [String]()
+
     var body: some View {
         GeometryReader { geometry in
-                    ScrollView(.vertical) {
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                    
+                    Text("Missions: ")
+                        .padding()
+                        .font(.headline)
+                    
+                    ForEach(self.missionsFlown, id: \.self) { mission in
                         VStack {
-                            Image(self.astronaut.id)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geometry.size.width)
-
-                            Text(self.astronaut.description)
-                                .padding()
+                            Text(mission.description)
+                                .font(.headline)
                         }
                     }
                 }
-                .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+    
+    init(astronaut: Astronaut) {
+        self.astronaut = astronaut
+        let missions: [Mission] = Bundle.main.decode("missions.json")
+        
+        var matches = [String]()
+        
+        for mission in missions {
+            for _ in mission.crew {
+                if let match = mission.crew.first(where: {$0.name == astronaut.id}) {
+                    matches.append("Apollo \(mission.id) - \(match.role)")
+                    break
+                }
+            }
+        }
+        self.missionsFlown = matches
     }
 }
 
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    @State var showDate = true
+    
     var body: some View {
         NavigationView {
             List(missions) { mission in
@@ -34,11 +36,25 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formattedLaunchDate)
+                        
+                        if self.showDate {
+                            Text(mission.formattedLaunchDate)
+                                .font(.subheadline)
+                        } else {
+                            Text(mission.showCrew(astronauts: astronauts))
+                                .font(.subheadline)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.showDate.toggle()
+                                    }, label: {
+                                        Text("Show \(self.showDate ? "crew" : "date")")
+                                    })
+            )
         }
     }
 }
