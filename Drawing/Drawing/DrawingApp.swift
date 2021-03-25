cf0af6b07dfea3a0d19ac0b71638a3d980636c16
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-25 20:57:18 +0300

    Create custom paths

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Татьяна Овсянникова on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
