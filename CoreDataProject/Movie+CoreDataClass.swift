5555e68c725011918bead6bd452bfe734db762af
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-04-30 21:19:58 +0300

    Add NSManagedObject subclass

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Татьяна Овсянникова on 30.04.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
