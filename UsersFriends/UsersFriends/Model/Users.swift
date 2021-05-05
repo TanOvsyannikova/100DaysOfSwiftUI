fe58834142f40e8faf5d685c7ab180781d24a766
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-05-05 21:51:23 +0300

    Fetch and parse data

--- UsersFriends/UsersFriends/Model/Users.swift
+++ UsersFriends/UsersFriends/Model/Users.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-
+class Users: ObservableObject {
+    @Published var all = [User]()
+    
+    init() {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let session = URLSession.shared.dataTask(with: request) { data, response, sessionError in
+            guard let data = data else {
+                print("Fetch failed: \(sessionError?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            do {
+                let decoded = try JSONDecoder().decode([User].self, from: data)
+                
+                DispatchQueue.main.async { [weak self] in
+                    self?.all = decoded
+                }
+            }
+            catch let decodingError {
+                print("Decoding failed: \(decodingError.localizedDescription)")
+            }
+        }
+        
+        DispatchQueue.global().async {
+            session.resume()
+        }
+        
+    }
+    
+}
